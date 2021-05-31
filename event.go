91d1715ccf14e7167a62a8abf5dd4260e2fe0fd4
Atul Bari <atul.bari@novanet.net>
2021-05-31 13:53:47 +0400

    static deps

--- event.go
+++ event.go
@@ -1,7 +1,8 @@
 package gst
 
 /*
-#cgo pkg-config: gstreamer-1.0
+#cgo CFLAGS: -I/usr/include/gstreamer-1.0/ -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include/
+#cgo LDFLAGS: -l:libgthread-2.0.a -l:libgio-2.0.a   -l:libgobject-2.0.a -l:libffi.a  -l:libgstreamer-full-1.0.a -l:libgmodule-2.0.a -lm -lunwind -ldl
 #include "gst.h"
 */
 import "C"
